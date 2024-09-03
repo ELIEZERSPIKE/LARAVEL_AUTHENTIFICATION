f6d2260bef849b99e15d9bc980e00a64a7d53dfa
ELIEZERSPIKE <amesseliezer@gmail.com>
2024-09-03 09:42:30 +0000

    premier commit

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import './index.css'
+import Registration from './Pages/Registration/Registration.jsx'
+import OtpCode from './Pages/OtpCode/OtpCode.jsx'
+import Dashboard from './Pages/Dashboard/Dashboard.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/registration",
+    element: <Registration />,
+  },
+  {
+    path: "/otp-code/:email",
+    element: <OtpCode />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
